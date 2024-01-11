40e14bb635b751b72bd893791a7da028ea7e7882
raneesa <raneesak73@gmail.com>
2024-01-11 09:56:19 +0530

    invoice

--- contoller/ordercontroller.js
+++ contoller/ordercontroller.js
@@ -202,7 +202,10 @@ const postinvoice = async (req, res) => {
         const htmlContent = invoiceData
 
 
-        const browser = await puppeteer.launch({ headless: "new" });
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        });
         const page = await browser.newPage();
 
 
